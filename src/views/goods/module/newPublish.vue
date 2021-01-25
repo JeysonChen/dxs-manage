0be4078b1e83dcc2d4fb601c4e2144644cf28cbe
chenqunxian01 <chenqunxian01@163.com>
2021-01-25 23:54:11 +0800

    shangchuan2

--- src/views/goods/module/newPublish.vue
+++ src/views/goods/module/newPublish.vue
@@ -39,38 +39,38 @@
                         <div class="item price-area">
                             <p class="item-title">产品价格</p>
                             <el-form-item prop="title" class="pl-52">
-                                <el-input size="small" v-model="input1">
+                                <el-input size="small" v-model="salePrice">
                                     <template slot="prefix">销售价：</template>
                                     <template slot="suffix">元</template>
                                 </el-input>
                             </el-form-item>
                             <el-form-item prop="subtitle">
-                                <el-input size="small" v-model="input1" >
+                                <el-input size="small" v-model="originPrice" >
                                     <template slot="prefix">原价：</template>
                                     <template slot="suffix">元</template>
                                 </el-input>
                             </el-form-item>
                             <el-form-item>
-                                <el-input size="small" v-model="input1">
+                                <el-input size="small" v-model="transportCosts">
                                     <template slot="prefix">运费：</template>
                                     <template slot="suffix">元</template>
                                 </el-input>
                             </el-form-item>
                             <p>分佣比例</p>
                             <el-form-item prop="title" class="pl-60">
-                                <el-input size="small" v-model="input1">
+                                <el-input size="small" v-model="groupOwnerEarnings">
                                     <template slot="prefix">大团主挣：</template>
                                     <template slot="suffix">%</template>
                                 </el-input>
                             </el-form-item>
                             <el-form-item prop="subtitle" class="pl-84">
-                                <el-input size="small" v-model="input1" >
+                                <el-input size="small" v-model="shareEarnings" >
                                     <template slot="prefix">小团主自转挣：</template>
                                     <template slot="suffix">%</template>
                                 </el-input>
                             </el-form-item>
                             <el-form-item class="pl-84">
-                                <el-input size="small" v-model="input1">
+                                <el-input size="small" v-model="helphairEarnings">
                                     <template slot="prefix">小团主帮发挣：</template>
                                     <template slot="suffix">%</template>
                                 </el-input>
@@ -82,30 +82,46 @@
                             <el-form-item>
                                 <el-button
                                     size="mini"
-                                    type="primary"
+                                    :type="categoryParentId === item.id ? 'primary' : ''"
                                     v-for="(item, index) in parentCatelogList"
                                     :key="index"
+                                    @click="catelogChange('categoryParentId', item)"
+                                >{{item.name}}</el-button>
+                            </el-form-item>
+                            <p>请选择二级分类</p>
+                            <el-form-item>
+                                <el-button
+                                    size="mini"
+                                    :type="categoryId === item.id ? 'primary' : ''"
+                                    v-for="(item, index) in parentCatelogList"
+                                    :key="index"
+                                    @click="catelogChange('categoryId', item)"
                                 >{{item.name}}</el-button>
                             </el-form-item>
                         </div>
                         <!-- 产品库存 -->
-                        <div class="item">
+                        <div class="item price-area">
                             <p class="item-title">产品库存</p>
-                            <el-form-item prop="title">
-                                
+                            <el-form-item prop="title" class="pl-52">
+                                <el-input size="small" v-model="stockQuantity">
+                                    <template slot="prefix">总数量：</template>
+                                    <template slot="suffix">件</template>
+                                </el-input>
                             </el-form-item>
-                            <el-form-item prop="subtitle">
-                               
+                            <el-form-item prop="subtitle" class="pl-114">
+                                <el-input size="small" v-model="maxBuyQuantity" >
+                                    <template slot="prefix">单人最多可购买数：</template>
+                                    <template slot="suffix">件</template>
+                                </el-input>
                             </el-form-item>
                         </div>
                         <!-- 大团主 -->
-                        <div class="item">
+                        <div class="item price-area">
                             <p class="item-title">大团主</p>
-                            <el-form-item prop="title">
-                                
-                            </el-form-item>
-                            <el-form-item prop="subtitle">
-                               
+                            <el-form-item prop="subtitle" class="pl-52">
+                                <el-input size="small" v-model="shareEarnings" >
+                                    <template slot="prefix">手机号：</template>
+                                </el-input>
                             </el-form-item>
                         </div>
                     </el-col>
@@ -113,12 +129,20 @@
                         <!-- 详情长图 -->
                         <div class="item">
                             <p class="item-title">详情长图</p>
-                            <el-form-item prop="title">
+                            <el-upload
+                                class="upload-detail"
+                                ref="upload"
+                                list-type="picture-card"
+                                action="https://jsonplaceholder.typicode.com/posts/"
+                                :on-preview="handlePreview"
+                                :on-remove="handleRemove"
+                                :file-list="fileList"
+                                :auto-upload="false">
+                                <i slot="default" class="el-icon-plus"></i>
+                                <div class="el-upload__text"><p class="title-one">上传详情长图</p></div>
                                 
-                            </el-form-item>
-                            <el-form-item prop="subtitle">
-                               
-                            </el-form-item>
+                                <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                            </el-upload>
                         </div>
                     </el-col>
                 </el-row>
@@ -126,54 +150,6 @@
             <div class="tox-bottom">
 
             </div>
-            
-            
-            <!-- <el-form-item label="活动名称" prop="name">
-                <el-input v-model="formData.name"></el-input>
-            </el-form-item>
-            <el-form-item label="活动区域" prop="region">
-                <el-select v-model="formData.region" placeholder="请选择活动区域">
-                <el-option label="区域一" value="shanghai"></el-option>
-                <el-option label="区域二" value="beijing"></el-option>
-                </el-select>
-            </el-form-item>
-            <el-form-item label="活动时间" required>
-                <el-col :span="11">
-                <el-form-item prop="date1">
-                    <el-date-picker type="date" placeholder="选择日期" v-model="formData.date1" style="width: 100%;"></el-date-picker>
-                </el-form-item>
-                </el-col>
-                <el-col class="line" :span="2">-</el-col>
-                <el-col :span="11">
-                <el-form-item prop="date2">
-                    <el-time-picker placeholder="选择时间" v-model="formData.date2" style="width: 100%;"></el-time-picker>
-                </el-form-item>
-                </el-col>
-            </el-form-item>
-            <el-form-item label="即时配送" prop="delivery">
-                <el-switch v-model="formData.delivery"></el-switch>
-            </el-form-item>
-            <el-form-item label="活动性质" prop="type">
-                <el-checkbox-group v-model="formData.type">
-                <el-checkbox label="美食/餐厅线上活动" name="type"></el-checkbox>
-                <el-checkbox label="地推活动" name="type"></el-checkbox>
-                <el-checkbox label="线下主题活动" name="type"></el-checkbox>
-                <el-checkbox label="单纯品牌曝光" name="type"></el-checkbox>
-                </el-checkbox-group>
-            </el-form-item>
-            <el-form-item label="特殊资源" prop="resource">
-                <el-radio-group v-model="formData.resource">
-                <el-radio label="线上品牌商赞助"></el-radio>
-                <el-radio label="线下场地免费"></el-radio>
-                </el-radio-group>
-            </el-form-item>
-            <el-form-item label="活动形式" prop="desc">
-                <el-input type="textarea" v-model="formData.desc"></el-input>
-            </el-form-item>
-            <el-form-item>
-                <el-button type="primary" @click="submitForm('ruleForm')">立即创建</el-button>
-                <el-button @click="resetForm('ruleForm')">重置</el-button>
-            </el-form-item> -->
         </el-form>
     </div>
 </template>
@@ -189,17 +165,32 @@ export default {
     data () {
         return {
             formData: {
-                // 标签
-                marks: [],
+                categoryId: 0,
+                categoryParentId: 0,
+                costPrice: 0,
+                details: '',
+                groupOwnerEarnings: 0,
+                groupOwnerId: 0,
+                helphairEarnings: 0,
+                mainImage: '',
+                maxBuyQuantity: 0,
+                monthlySales: 0,
                 name: '',
-                region: '',
-                date1: '',
-                date2: '',
-                delivery: false,
-                type: [],
-                resource: '',
-                desc: ''
+                originPrice: 0,
+                promotionIds: [],
+                salePrice: 0,
+                shareEarnings: 0,
+                status: 0,
+                stockQuantity: 0,
+                stockStatus: 0,
+                strategyIds: [],
+                subImages: '',
+                subtitle: '',
+                tagIds: [],
+                transportCosts: 0
             },
+            categoryParentId: 0, // 父类当前项
+            categoryId: 0, // 子类当前项
             marks: [], // 标签列表
             parentCatelogList: [], // 一级分类
             subCatelogList: [], // 二级分类
@@ -248,7 +239,13 @@ export default {
             Api.product.getCategoryList().then(res => {
                 console.log(res, 'fenlei');
                 this.parentCatelogList = res.data.filter(item => item.parentId === 0);
+                this.categoryParentId = this.parentCatelogList[0].id;
             })
+        },
+        // 分类
+        catelogChange(type, item) {
+            this.formData[type] = item.id;
+            this[type] = item.id;
         }
     }
 }
